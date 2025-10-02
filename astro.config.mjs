ab5d610dcce6c906c706470b456198e6e15aea1c
yukiya <ay01sec@gmail.com>
2025-10-03 01:09:54 +0900

    first

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap'
 
-// https://astro.build/config
-export default defineConfig({});
+
+export default defineConfig({
+site: 'https://YOUR-DOMAIN.example', // 後で独自ドメインに差し替え
+integrations: [mdx(), sitemap()],
+trailingSlash: 'never',
+})
